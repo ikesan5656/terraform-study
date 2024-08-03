bcb68a029ee12bdb50b30fe1c81b297f59482273
池ノ谷優太 <you1207.ike@gmail.com>
2024-08-03 10:28:44 +0900

    構成変更

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = var.region
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile = var.profile
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
--- variables.tf
+++ variables.tf
@@ -10,3 +10,12 @@ variable "region" {
 variable "profile" {
   type = string
 }
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Key"
+  type        = string
+}
